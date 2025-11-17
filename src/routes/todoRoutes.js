daf621547675022a165b0ce9b77448a0f6ea2750
N-PanhaMony <neangpanhamony001@gmail.com>
2025-11-17 21:26:22 +0700

    modify auth/todo routes

---
+++ src/routes/todoRoutes.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import db from '../db.js'
+
+const router = express.Router()
+
+//Get all todos
+router.get('/', (req , res ) =>{
+
+})
+
+// Create new todo
+router.post('/' , (req ,res ) =>{
+
+})
+
+//Update a todo
+router.put('/:id', (req ,res) =>{
+
+})
+
+//Delete a toda
+router.delete('/:id' , (req ,res ) =>{
+
+})
+
+export default router
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import path, {dirname} from 'path'
 import { fileURLToPath } from 'url'
+import authRoutes from './routes/authRoutes.js'
+import todoRoutes from './routes/todoRoutes.js'
 
 const app = express()
 const PORT = process.env.PORT || 5001
@@ -20,6 +22,9 @@ app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
+//Routes
+app.use('/auth',authRoutes)
+app.use('/todos',todoRoutes)
 
 app.listen(PORT , ()=>{
     console.log(`server run on port :${PORT}`);
